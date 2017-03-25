64e4d3c70d7221b76e6c4811b3d33a379a0206f9
Shane Mc Cormack <shanemcc@gmail.com>
2017-03-25 03:08:42 +0000

    Missing file

---
+++ api-config.local.php
@@ -0,0 +1,5 @@
+<?php
+	$config['hooks']['bind']['enabled'] = 'true';
+	$config['hooks']['bind']['catalogZoneFile'] = '/bind/catalog.db';
+	$config['hooks']['bind']['catalogZoneName'] = 'catalog.invalid';
+	$config['hooks']['bind']['zonedir'] = '/bind/zones';
