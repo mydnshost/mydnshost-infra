f2fde70145766a2ab3697518c773e7d4aadc9128
Shane Mc Cormack <shanemcc@gmail.com>
2017-09-06 01:38:09 +0100

    Add statuscake updater.

---
+++ maintenance/scripts/init.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /scripts/statuscake-updater
+composer install
---
+++ maintenance/scripts/statuscake-updater/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+	function getEnvOrDefault($var, $default) {
+		$result = getEnv($var);
+		return $result === FALSE ? $default : $result;
+	}
+
+	require_once(dirname(__FILE__) . '/config.php');
+	require_once(__DIR__ . '/vendor/autoload.php');
