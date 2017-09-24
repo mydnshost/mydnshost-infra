70265e9285a684aaacb37676164b0efd604cb6fc
Shane Mc Cormack <shanemcc@gmail.com>
2017-09-24 21:31:12 +0100

    Add NSEC3 records.

--- maintenance/scripts/dnssec.sh
+++ maintenance/scripts/dnssec.sh
@@ -26,7 +26,7 @@ ls -1 /bind/zones/ | while read ZONE; do
 
 			${RNDC} loadkeys ${ZONE}
 			${RNDC} sign ${ZONE}
-			${RNDC} signing -nsec3param 1 0 10 $(head -c 512 /dev/urandom | sha1sum | cut -b 1-16)
+			${RNDC} signing -nsec3param 1 0 10 $(head -c 512 /dev/urandom | sha1sum | cut -b 1-16) ${ZONE}
 		fi;
 
 		if [ ! -e "/bind/keys/${ZONE}.dskey" ]; then
