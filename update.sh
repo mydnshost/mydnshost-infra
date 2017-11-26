1cbdc8d053172e478e852a7c02eb9bfa7830e72e
Shane Mc Cormack <shanemcc@gmail.com>
2017-11-26 21:13:09 +0000

    Improve service restarting.

--- update.sh
+++ update.sh
@@ -42,12 +42,48 @@ docker pull mydnshost/mydnshost-frontend
 docker pull mydnshost/mydnshost-bind
 docker pull mydnshost/mydnshost-docker-cron
 
-# Rebuild any running containers if needed.
-for IMAGE in api web bind maintenance; do
+api_VERSION=`docker inspect $(docker images mydnshost/mydnshost-api --format "{{.ID}}") | "${DIR}/maintenance/scripts/jq" .[0].Id`
+web_VERSION=`docker inspect $(docker images mydnshost/mydnshost-frontend --format "{{.ID}}") | "${DIR}/maintenance/scripts/jq" .[0].Id`
+
+# Rebuild running stateless containers if needed by scaling up then killing off the older containers.
+for IMAGE in api web; do
         RUNNING=`docker-compose ps "${IMAGE}" | grep " Up "`
         if [ "" != "${RUNNING}" ]; then
-                docker-compose up -d --no-deps "${IMAGE}"
+		NEED_UPGRADE=0;
+		docker-compose ps "${IMAGE}" | grep " Up " | awk '{print $1}' | while read NAME; do
+			ID=`docker ps --filter name="${NAME}" --format {{.ID}}`
+			MY_VERSION=`docker inspect "${ID}" | "${DIR}/maintenance/scripts/jq" .[0].Image`
+			IMAGE_VERSION="${IMAGE}_VERSION"
+
+			if [ "${MY_VERSION}" != "${!IMAGE_VERSION}" ]; then
+				echo "${NAME} needs upgrading."
+				NEED_UPGRADE=1
+			else
+				echo "${NAME} is up to date."
+			fi;
+		done;
+
+		if [ "${NEED_UPGRADE}" = "1" ]; then
+			# Scale up to 2 to start new container.
+	                docker-compose up -d --no-deps --scale "${IMAGE}"=2 "${IMAGE}"
+
+			# Kill off older containers.
+			docker-compose ps "${IMAGE}" | grep " Up " | head -n -1 | awk '{print $1}' | while read NAME; do
+				echo 'Stopping older container: '"${NAME}";
+				ID=`docker ps --filter name="${NAME}" --format {{.ID}}`
+				docker stop "${ID}"
+				docker rm -f "${ID}"
+			done;
+		fi;
         fi;
+done;
+
+# Rebuild any single-instance containers if needed.
+for IMAGE in bind maintenance; do
+	RUNNING=`docker-compose ps "${IMAGE}" | grep " Up "`
+	if [ "" != "${RUNNING}" ]; then
+		docker-compose up -d --no-deps "${IMAGE}"
+	fi;
 done;
 
 docker-compose up -d
@@ -55,7 +91,9 @@ docker-compose up -d
 echo "Waiting for start..."
 sleep 10;
 
-docker exec -it mydnshost_api ln -sf /dnsapi/examples/hooks/bind_workers.php /dnsapi/hooks/bind.php
-docker exec -it mydnshost_api ln -sf /dnsapi/examples/hooks/webhook_workers.php /dnsapi/hooks/webhook.php
-docker exec -it mydnshost_api chown www-data: /bind
-docker exec -it mydnshost_api su www-data --shell=/bin/bash -c "/dnsapi/admin/init.php"
+docker ps -a --format '{{.Names}}' | grep -i mydnshost_api_ | while read NAME; do
+	docker exec -t "${NAME}" ln -sf /dnsapi/examples/hooks/bind_workers.php /dnsapi/hooks/bind.php
+	docker exec -t "${NAME}" ln -sf /dnsapi/examples/hooks/webhook_workers.php /dnsapi/hooks/webhook.php
+	docker exec -t "${NAME}" chown www-data: /bind
+	docker exec -t "${NAME}" su www-data --shell=/bin/bash -c "/dnsapi/admin/init.php"
+done;
