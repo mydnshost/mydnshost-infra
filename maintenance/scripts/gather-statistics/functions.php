c90fd913ff5f25514254c524ad436f24fe143a44
Shane Mc Cormack <shanemcc@gmail.com>
2019-07-31 01:53:23 +0100

    Move maintenance scripts to separate container/repo

--- maintenance/scripts/gather-statistics/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	function getEnvOrDefault($var, $default) {
-		$result = getEnv($var);
-		return $result === FALSE ? $default : $result;
-	}
-
-	require_once(dirname(__FILE__) . '/config.php');
-	require_once(__DIR__ . '/vendor/autoload.php');
--- maintenance/scripts/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /scripts/statuscake-updater
-composer install
-
-cd /scripts/gather-statistics
-composer install
